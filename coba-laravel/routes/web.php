d8f75700216421caf8958685c61e302b4a51cd87
unknown <muhammadafrizalfadillah@gmail.com>
2022-10-21 17:15:00 +0700

    Struktur Folder, Router dan View

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Muhammad Afrizal Fadillah",
+        "email" => "ijal@gmail.com",
+        "image" => "ijal.jpg"
+    ]);
+});
+Route::get('/blog', function () {
+    return view('posts');
 });
