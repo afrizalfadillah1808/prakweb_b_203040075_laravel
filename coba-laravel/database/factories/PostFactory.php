5a035a77361e471c6246b8445d3bb672d7a4c72c
unknown <muhammadafrizalfadillah@gmail.com>
2022-10-30 20:27:10 +0700

    Factory dan Faker

--- coba-laravel/database/factories/PostFactory.php
+++ coba-laravel/database/factories/PostFactory.php
@@ -17,7 +17,12 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => fake()->sentence(mt_rand(2, 8)),
+            'slug' => fake()->slug(),
+            'excerpt' => fake()->paragraph(),
+            'body' => fake()->paragraph(mt_rand(5, 10)),
+            'user_id' => mt_rand(1,3),
+            'category_id' => mt_rand(1,2)
         ];
     }
 }
