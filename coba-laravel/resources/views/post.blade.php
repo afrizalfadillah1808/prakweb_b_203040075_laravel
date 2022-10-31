b92f07484cd599cd1216b6aed60cc13fb7f18d8c
unknown <muhammadafrizalfadillah@gmail.com>
2022-10-31 08:45:37 +0700

    Membetulkan yang error pada author

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.main')
 
 @section('container')
+<article>
+    <h2 class="mb-5">{{ $post->title }}</h2>
 
-    <article>
-        <h2>{{ $post->title }}</h2>
+    <p>By. <a href="/authors/{{ $post->author->username }}" class='text-decoration-none'>{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class='text-decoration-none'>{{ $post->category->name }}</a></p>
 
-        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+    {!! $post->body !!}
 
-        {!! $post->body !!}
-    </article>
 
-    <a href="/blog" class="d-block mt-5">Back to Post</a>
+</article>
 
+<a href="/blog" class="d-block mt-3">Back to Posts</a>
 @endsection
