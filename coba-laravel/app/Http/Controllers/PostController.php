20f80b22a63867d97336e33cfae82668be0fb8df
unknown <muhammadafrizalfadillah@gmail.com>
2022-11-04 18:44:20 +0700

    Searching dan Pagination

--- coba-laravel/app/Http/Controllers/PostController.php
+++ coba-laravel/app/Http/Controllers/PostController.php
@@ -2,18 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\User;
 
 class PostController extends Controller
 {
     public function index()
     {
+        $title = '';
+        if(request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
+        if(request('author')) {
+            $author = User::firstWhere('slug', request('author'));
+            $title = ' by ' . $author->name;
+        }
         return view('posts', [
             "title" => "All Posts",
             "active" => 'posts',
             // "posts" => Post::all()
-            "posts" => Post::latest()->get()
+            "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
         ]);
     }
 
--- coba-laravel/resources/views/categories.blade.php
+++ coba-laravel/resources/views/categories.blade.php
@@ -9,11 +9,11 @@
             <div class="row">
                 @foreach ($categories as $category)
                     <div class="col-md-4">
-                        <a href="/categories/{{ $category->slug }}">
+                        <a href="/posts?category={{ $category->slug }}">
                             <div class="card bg-dark text-white">
-                                <img class="card-img" src="https://source.unsplash.com/1200x400?{{ $category->name }}" alt="{{ $category->name }}">
+                                <img class="card-img" src="https://source.unsplash.com/500x500?{{ $category->name }}" alt="{{ $category->name }}">
                                 <div class="card-img-overlay d-flex align-items-center p-0">
-                                <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 7)">{{ $category->name }}</h5>
+                                <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0.7)">{{ $category->name }}</h5>
                                 </div>
                             </div>
                         </a>
