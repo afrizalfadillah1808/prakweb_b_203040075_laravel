f41a04f26344bfdc005038b7893a1157da2039b1
unknown <muhammadafrizalfadillah@gmail.com>
2022-11-11 14:18:35 +0700

    User Login dan Middleware

--- coba-laravel/resources/views/partials/navbar.blade.php
+++ coba-laravel/resources/views/partials/navbar.blade.php
@@ -18,12 +18,35 @@
           <a class="nav-link {{ ($active === "categories") ? 'active' : '' }}" href="/categories">Categories</a>
         </li>
       </ul>
+
+
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-            <a href="/login" class="nav-link {{ $active === 'login' ? 'active' : '' }}"><i
-                    class="bi bi-box-arrow-in-right"></i> Login</a>
-        </li>
+          @auth
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Welcome Back, {{ auth()->user()->name }}
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>
+                  <a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a>
+                </li>
+                <li class="dropdown-divider"></li>
+                <li>
+                  <form action="/logout" method="post">
+                    @csrf
+                    <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout</button>
+                  </form>
+                </li>
+              </ul>
+            </li>
+          @else
+          <li class="nav-item">
+              <a href="/login" class="nav-link {{ $active === 'login' ? 'active' : '' }}"><i
+                      class="bi bi-box-arrow-in-right"></i> Login</a>
+          </li>
+          @endauth
       </ul>
+
 
     </div>
 </nav>
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
@@ -65,7 +66,11 @@ Route::get('/authors/{author:username}', function (User $author) {
     ]);
 });
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
