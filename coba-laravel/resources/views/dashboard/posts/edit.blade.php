c0cb1064eae5a20eb177b4d16da40bada79ecf13
unknown <muhammadafrizalfadillah@gmail.com>
2022-11-27 13:06:54 +0700

    Preview, Update & Delete Image

--- coba-laravel/resources/views/dashboard/posts/edit.blade.php
+++ coba-laravel/resources/views/dashboard/posts/edit.blade.php
@@ -6,7 +6,7 @@
   </div>
 
     <div class="col-lg-8">
-        <form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5">
+        <form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5" enctype="multipart/form-data">
            @method('put')
            
             @csrf
@@ -44,6 +44,25 @@
                     @endforeach
                 </select>
             </div>
+
+            <div class="mb-3">
+                <label for="image" class="form-label">Post Image</label>
+
+                <input type="hidden" name="oldImage" value="{{ $post->image }}">
+
+                @if ($post->image)
+                <img src="{{ asset('storage/' . $post->image) }}" class="img-preview img-fluid mb-3 col-sm-5 d-block">   
+                @else
+                    <img class="img-preview img-fluid mb-3 col-sm-5"> 
+                @endif
+                <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image" onchange="previewImage()">
+                @error('image')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div>
+                @enderror
+            </div>
+
             <div class="mb-3">
                 <label for="body" class="form-label">Body</label>
                 @error('body')
@@ -74,7 +93,23 @@
 
         document.addEventListener('trix-file-accept', function(e) {
             e.preventDefault();
+
         })
+
+        function previewImage() {
+            const image = document.querySelector('#image');
+            const imgPreview = document.querySelector('.img-preview');
+
+            imgPreview.style.display = 'block';
+
+            const oFReader = new FileReader();
+            oFReader.readAsDataURL(image.files[0]);
+
+            oFReader.onload = function(oFEvent) {
+                imgPreview.src = oFEvent.target.result;
+            }
+
+        }
 
     </script>
 @endsection
